8bc6410b9e23b1f7f3f1bec8c21bb5f0592038bc
jonathon Zachariah <jzac90@gmail.com>
2025-12-12 15:44:43 +0000

    pulumi setup

---
+++ __main__.py
@@ -0,0 +1,35 @@
+import pulumi
+import pulumi_aws as aws
+
+# Create an S3 bucket
+bucket = aws.s3.Bucket("klaviyo-events-bucket")
+
+# Create a Lambda function
+role = aws.iam.Role("lambdaRole",
+    assume_role_policy="""{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "lambda.amazonaws.com"
+            }
+        }
+    ]
+}""")
+
+policy = aws.iam.RolePolicyAttachment("lambdaPolicy",
+    role=role.id,
+    policy_arn="arn:aws:iam::aws:policy/AWSLambdaExecute")
+
+lambda_function = aws.lambda_.Function("klaviyo-event-grab-lambda",
+    role=role.arn,
+    runtime="python3.13",
+    handler="index.handler",
+    code=pulumi.AssetArchive({
+        '.': pulumi.FileArchive('./lambda')
+    }))
+
+pulumi.export("bucket_name", bucket.bucket)
+pulumi.export("lambda_function_name", lambda_function.name)
