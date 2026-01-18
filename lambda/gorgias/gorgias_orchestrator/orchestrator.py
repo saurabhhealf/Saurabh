571c8c143a24da92226f0e15abe028a40a2003d2
Saurabh <saurabh.healf.com>
2026-01-18 22:45:02 +0530

    made changes in grogias41

--- lambda/gorgias/gorgias_orchestrator/orchestrator.py
+++ lambda/gorgias/gorgias_orchestrator/orchestrator.py
@@ -76,6 +76,14 @@ def handler(event, context):
                 "expected_hour": daily_start_hour,
             }
 '''
+        if STREAM_NAME != "customers" and now_dt.hour != daily_start_hour:
+                return {
+                    "enqueued": False,
+                    "reason": "outside_daily_window",
+                    "stream": STREAM_NAME,
+                    "current_hour": now_dt.hour,
+                    "expected_hour": daily_start_hour,
+                }
         # Calculate Target Date
         target_date = (now_dt - timedelta(days=days_back)).strftime("%Y-%m-%d")
 
